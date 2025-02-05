4ae8b5cfa2284df47e9da51dc08eca7c1b4db9cb
Abhideep <itsabhideep@gmail.com>
2025-02-06 01:58:37 +0530

    using pages try 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -7,4 +8,7 @@ const nextConfig = {
   },
 };
 
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 export default nextConfig;
